eb7c1430db63456e87d7d7e4b86d2f5735e13702
Santiago Silvestre <silvestre.santiago732@gmail.com>
2026-08-05 22:00:17 -0300

    feat: initialize spring boot project setupfeat: initialize spring boot project setup

---
+++ src/test/java/br/com/fiap/workshop_management_system/WorkshopManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.workshop_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
